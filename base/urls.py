f0c1a280ab4205b306b3a2fe80eb97078510b062
Ivan Amorós <ivan13ap@gmail.com>
2025-09-18 16:08:23 +0200

    cleaning the project just leaving the film festival part

--- base/urls.py
+++ base/urls.py
@@ -1,15 +1,6 @@
 from django.urls import path
 from .views import (
-    CommentList,
-    TechnicalSkillCategoryList,
-    WorkExperienceList,
     ValidateTokenView,
-    TechnicalSkillCategoryCreate,
-    TechnicalSkillCreate,
-    TechnicalSkillUpdate,
-    TechnicalSkillCategoryUpdate,
-    StudyList,
-    ProjectList,
     MyTokenObtainPairView,
     UserRegistrationAPIView,
     PasswordResetView,
@@ -19,22 +10,9 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
-	path('skills/', TechnicalSkillCategoryList.as_view()),
-	path('categories/create/', TechnicalSkillCategoryCreate.as_view(), name='category-create'),
-	path('categories/<int:pk>/', TechnicalSkillCategoryUpdate.as_view(), name='technical-skill-category-update'),
-	path('skills/create/', TechnicalSkillCreate.as_view(), name='skill-create'),
-	path('skills/<int:pk>/', TechnicalSkillUpdate.as_view(), name='technical-skill-update'),
 
-	path('works/', WorkExperienceList.as_view()),
-
-	path('studies/', StudyList.as_view()),
-
-	path('comments/', CommentList.as_view()),
-    
-        path('projects/', ProjectList.as_view()),
-
-        path('api/password-reset/', PasswordResetView.as_view(), name='password-reset'),
-        path('reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    path('api/password-reset/', PasswordResetView.as_view(), name='password-reset'),
+    path('reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
 
 	path('api/register/', UserRegistrationAPIView.as_view(), name='register'),
 	path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
